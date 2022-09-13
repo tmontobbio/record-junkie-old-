be5627c31710b5520057c5b56b5f92bb8f2a5b2f
Tyler <toygrrr@gmail.com>
2022-09-13 11:36:20 -0700

    layout for about page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,6 +1,98 @@
 import React from "react"
 import "./AboutUs.css"
+import { Container, Header } from "semantic-ui-react"
 
 export default function AboutUs() {
-	return <div id="about-us">About Us</div>
+	return (
+		<div id="about-us">
+			<div id="pos-1">
+				<img
+					src="https://www.gannett-cdn.com/media/2019/09/13/USATODAY/usatsports/247WallSt.com-247WS-577225-imageforentry6-7yr.jpg"
+					alt="record-player"
+					className="about-image"
+				/>
+			</div>
+			<div id="pos-2">
+				<Container text>
+					<Header as="h2">Header</Header>
+					<p>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+						commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+						penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+						Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+						Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+						aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+						imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+						link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+						elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+						ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+						lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+						viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+						imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+						ultricies nisi.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+						commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+						penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+						Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+						Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+						aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+						imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+						link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+						elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+						ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+						lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+						viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+						imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+						ultricies nisi.
+					</p>
+				</Container>
+			</div>
+			<div id="pos-3">
+				<Container text>
+					<Header as="h2">Header</Header>
+					<p>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+						commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+						penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+						Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+						Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+						aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+						imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+						link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+						elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+						ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+						lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+						viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+						imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+						ultricies nisi.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+						commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+						penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+						Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+						Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+						aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+						imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+						link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+						elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+						ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+						lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+						viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+						imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+						ultricies nisi.
+					</p>
+				</Container>
+			</div>
+			<div id="pos-4">
+				<img
+					src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTIaggB6msfZAgThejXGRqo4cTw8cL9IGjawwPyKLCnSXMuK28jKGkqtZkrpLmq6-B3cwBtYLnUepdRCBlJubs"
+					alt="mixer"
+					className="about-image"
+				/>
+			</div>
+		</div>
+	)
 }
